3ea71d15097af68b5da7e9335780f09e64ea6d36
enxxxi <enxix226@gmail.com>
2024-11-26 22:22:43 +0800

    helloworld java

---
+++ Hackathon.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hackathon;
+
+/**
+ *
+ * @author SCSM11
+ */
+public class Hackathon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Prints Hello World to the console
+        System.out.println("Hello, World!");
+    }
+}
+
+    
+    
+
